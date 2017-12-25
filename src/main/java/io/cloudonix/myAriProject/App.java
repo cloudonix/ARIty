82235457a2c0ca783ced6121ee38249aa539a407
Naama Gertel <naama,gertel@cloudonix.io>
2017-12-25 18:13:28 +0200

    from StasisStart get the Channel and it's id answer a call play "hello-world" in a sync way

--- src/main/java/io/cloudonix/myAriProject/App.java
+++ src/main/java/io/cloudonix/myAriProject/App.java
@@ -6,10 +6,12 @@ import java.util.logging.Logger;
 import ch.loway.oss.ari4java.ARI;
 import ch.loway.oss.ari4java.generated.ActionAsterisk;
 import ch.loway.oss.ari4java.generated.ActionChannels;
+import ch.loway.oss.ari4java.generated.ActionEvents;
 //import ch.loway.oss.ari4java.cfg.AriVersion;
 import ch.loway.oss.ari4java.generated.AsteriskInfo;
 import ch.loway.oss.ari4java.generated.Channel;
 import ch.loway.oss.ari4java.generated.Message;
+import ch.loway.oss.ari4java.generated.Playback;
 import ch.loway.oss.ari4java.generated.StasisStart;
 import ch.loway.oss.ari4java.tools.ARIException;
 import ch.loway.oss.ari4java.tools.AriCallback;
@@ -23,31 +25,70 @@ import ch.loway.oss.ari4java.AriVersion;
  */
 public class App {
 	private final static Logger logger = Logger.getLogger(App.class.getName());
-	
+
 	public static void main(String[] args) {
 
 		try {
 
-			ARI ari = AriFactory.nettyHttp("http://127.0.0.1:8088/", "userid", "secret", AriVersion.ARI_1_5_0);
-			ari.events().eventWebsocket("stasisAPP", new AriCallback<Message>() {
+			ARI ari = AriFactory.nettyHttp("http://127.0.0.1:8088/", "userid", "secret", AriVersion.ARI_2_0_0);
+			ari.events().eventWebsocket("stasisAPP", true, new AriCallback<Message>() {
 
 				@Override
 				public void onSuccess(Message result) {
 					String as_id = result.getAsterisk_id();
 					logger.info("success! Asterisk id: " + as_id);
+
+					if (!(result instanceof StasisStart))
+						// if result is not StasisStart, ignore it
+						return;
+
+					// result is StasisStart
+					StasisStart event = (StasisStart) result;
+					Channel currChannel = event.getChannel();
+					String channID = currChannel.getId();
+					logger.info("the channel id is:" + channID);
+
+					// answer the call
 					try {
-						ari.getWebsocketQueue();
-					} catch (ARIException e) {
+						ari.channels().answer(channID);
+						
+						// play syn
+						Playback pb = ari.channels().play(channID, "sound:hello-world", currChannel.getLanguage(), 0, 0, "");
+						logger.info("play" + pb.getMedia_uri());
+						
+						/*// play ansync
+						ari.channels().play(channID, "sound:hello-world",currChannel.getLanguage(), 0, 0, new AriCallback<Playback>() {
+
+							@Override
+							public void onSuccess(Playback resultM) {
+								
+								if(!(resultM instanceof Playback))
+									return;
+								
+								// hangup call
+								logger.info("hangup my call");
+								try {
+									ari.channels().hangup(channID, "call ending");
+								} catch (RestException e) {
+									// TODO Auto-generated catch block
+									e.printStackTrace();
+								}
+
+							}
+
+							@Override
+							public void onFailure(RestException e) {
+								// TODO Auto-generated method stub
+								e.printStackTrace();
+							}
+							
+						});
+
+						*/
+
+					} catch (RestException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
-					}
-
-					StasisStart ss;
-					try {
-						ss = ari.getActionImpl(StasisStart.class);
-						Channel currChannel = ss.getChannel();
-						String channID = currChannel.getId();
-						int x = 3;
 					} catch (ARIException e) {
 						// TODO Auto-generated catch block
 						e.printStackTrace();
