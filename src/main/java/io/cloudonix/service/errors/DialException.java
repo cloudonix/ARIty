55ff59f27320bd66df21ed89945e73ff86aecf93
Naama Gertel <naama,gertel@cloudonix.io>
2018-01-09 18:55:57 +0200

    add dial exception

---
+++ src/main/java/io/cloudonix/service/errors/DialException.java
@@ -0,0 +1,15 @@
+package io.cloudonix.service.errors;
+
+public class DialException extends Exception {
+	
+private static final long serialVersionUID = 1L;
+	
+	public DialException() {
+		super();
+	}
+	
+	public DialException (Throwable t) {
+		super(t);
+	}
+
+}
