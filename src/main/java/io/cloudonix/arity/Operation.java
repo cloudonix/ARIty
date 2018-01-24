f3234617055053b8c45ea5c61c0147d5ff0173ad
Naama Gertel <naama,gertel@cloudonix.io>
2018-01-24 19:20:28 +0200

    Added utilities

--- src/main/java/io/cloudonix/arity/Operation.java
+++ src/main/java/io/cloudonix/arity/Operation.java
@@ -1,8 +1,11 @@
 package io.cloudonix.arity;
 
 import java.util.concurrent.CompletableFuture;
+import java.util.function.Consumer;
 
 import ch.loway.oss.ari4java.ARI;
+import ch.loway.oss.ari4java.tools.AriCallback;
+import ch.loway.oss.ari4java.tools.RestException;
 
 public abstract class Operation {
 
@@ -29,5 +32,32 @@ public abstract class Operation {
 	}
 
 	abstract CompletableFuture<? extends Operation> run();
+
+	protected <V> CompletableFuture<V> toFuture(Consumer<AriCallback<V>> op) {
+		CompletableFuture<V> cf = new CompletableFuture<V>();
+		AriCallback<V> ariCallback = new AriCallback<V>() {
+
+			@Override
+			public void onSuccess(V result) {
+				cf.complete(result);
+			}
+
+			@Override
+			public void onFailure(RestException e) {
+				cf.completeExceptionally(e);
+			}
+		};
+		
+		op.accept(ariCallback);
+		return cf;
+		
+	}
+
+	public static <V> CompletableFuture<V> completedExceptionally(Throwable th) {
+		
+		CompletableFuture<V> compExceptionaly = new CompletableFuture<V>();
+		 compExceptionaly.completeExceptionally(th);
+		 return compExceptionaly;
+	}
 
 }
