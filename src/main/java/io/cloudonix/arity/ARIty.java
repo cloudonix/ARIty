2006cc90a232609f01c5f1517ae31762fa030fab
Naama Gertel <naama,gertel@cloudonix.io>
2018-03-20 11:10:26 +0200

    changed back to I'm feeling lucky

--- src/main/java/io/cloudonix/arity/ARIty.java
+++ src/main/java/io/cloudonix/arity/ARIty.java
@@ -62,7 +62,7 @@ public class ARIty implements AriCallback<Message> {
 		appName = name;
 
 		try {
-			ari = ARI.build(uri, appName, login, pass, AriVersion.ARI_2_0_0);
+			ari = ARI.build(uri, appName, login, pass, AriVersion.IM_FEELING_LUCKY);
 			logger.info("ari created");
 			logger.info("ari version: " + ari.getVersion());
 			ari.events().eventWebsocket(appName, true, this);
