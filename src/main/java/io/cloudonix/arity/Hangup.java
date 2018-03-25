ce7bcc0e6586509e0d9f9ccb02070c265aad9572
Naama Gertel <naama,gertel@cloudonix.io>
2018-03-25 08:51:13 +0300

    correct description of a method

--- src/main/java/io/cloudonix/arity/Hangup.java
+++ src/main/java/io/cloudonix/arity/Hangup.java
@@ -17,7 +17,7 @@ public class Hangup extends Operation{
 	
 	/**
 	 * Constructor
-	 * @param callController
+	 * @param callController instance that represents a call
 	 */
 	public Hangup(CallController callController) {
 		super(callController.getChannelID(), callController.getARItyService(), callController.getAri());
