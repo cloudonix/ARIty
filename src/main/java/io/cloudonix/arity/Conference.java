0eeb246d9e505dfc25eec3782f3db2d550149c2e
Naama Gertel <naama,gertel@cloudonix.io>
2018-04-11 17:04:03 +0300

    add channel to a conference only if conference state is Ready

--- src/main/java/io/cloudonix/arity/Conference.java
+++ src/main/java/io/cloudonix/arity/Conference.java
@@ -2,6 +2,7 @@ package io.cloudonix.arity;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
@@ -12,11 +13,11 @@ import ch.loway.oss.ari4java.tools.RestException;
 import io.cloudonix.arity.errors.ErrorStream;
 
 public class Conference extends Operation {
-	
-	public enum ConferenceState
-    {
-        Destroyed, Destroying ,Creating, Ready, ReadyWaiting, Muted, AdminMuted 
-    }
+
+	public enum ConferenceState {
+		Destroyed, Destroying, Creating, Ready, ReadyWaiting, Muted, AdminMuted
+	}
+
 	private CompletableFuture<Conference> compFuture;
 	private ConferenceState currState;
 	private Bridge confBridge;
@@ -26,7 +27,6 @@ public class Conference extends Operation {
 	private String confId;
 	private final static Logger logger = Logger.getLogger(Conference.class.getName());
 
-	
 	public Conference(String confId, ARIty arity, ARI ari) {
 		super(confId, arity, ari);
 		this.confId = confId;
@@ -38,110 +38,137 @@ public class Conference extends Operation {
 	public CompletableFuture<? extends Operation> run() {
 		return null;
 	}
-	
+
 	/**
 	 * get conference currenr state
+	 * 
 	 * @return
 	 */
 	public ConferenceState getCurrState() {
 		return currState;
 	}
-	
+
 	/**
 	 * set conference state
-	 * @param currState  update sate of conference
-	 * 	 */
+	 * 
+	 * @param currState
+	 *            update sate of conference
+	 */
 	public void setCurrState(ConferenceState currState) {
 		this.currState = currState;
 	}
+
 	/**
 	 * get conference bridge
+	 * 
 	 * @return
 	 */
 	public Bridge getConfBridge() {
 		return confBridge;
 	}
+
 	/**
 	 * set conference bridge
+	 * 
 	 * @return
 	 */
 	public void setConfBridge(Bridge confBridge) {
 		this.confBridge = confBridge;
 	}
+
 	/**
 	 * get conference name
+	 * 
 	 * @return
 	 */
 	public String getConfName() {
 		return confName;
 	}
+
 	/**
 	 * set conference name
+	 * 
 	 * @return
 	 */
 	public void setConfName(String confName) {
 		this.confName = confName;
 	}
-	
+
 	/**
 	 * get number of channels in conference
+	 * 
 	 * @return
 	 */
 	public int getCount() {
 		return count;
 	}
+
 	/**
 	 * set number of channels in conference
+	 * 
 	 * @return
 	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
-	
+
 	/**
 	 * get list of channels connected to the conference bridge
+	 * 
 	 * @return
 	 */
 	public List<Channel> getChannelsInConf() {
 		return channelsInConf;
 	}
+
 	/**
 	 * set list of channels connected to the conference bridge
+	 * 
 	 * @return
 	 */
 	public void setChannelsInConf(List<Channel> channelsInConf) {
 		this.channelsInConf = channelsInConf;
 	}
+
 	/**
 	 * add channel to the conference
+	 * 
 	 * @param channel
 	 * @param cc
 	 */
-	public void addChannelToConf (Channel channel, CallController cc) {
-		try {
-			cc.getAri().bridges().addChannel(confBridge.getId(), channel.getId(), "joining conference");
-		} catch (RestException e) {
-			logger.severe("unable to add channel to conference " +ErrorStream.fromThrowable(e));
-			return;
+	public void addChannelToConf(Channel channel, CallController cc) {
+		
+		if (Objects.equals(currState, ConferenceState.Ready)) {
+			try {
+				cc.getAri().bridges().addChannel(confBridge.getId(), channel.getId(), "joining conference");
+			} catch (RestException e) {
+				logger.severe("unable to add channel to conference " + ErrorStream.fromThrowable(e));
+				return;
+			}
+			channelsInConf.add(channel);
+			count++;
+			if (count == 1) {
+				logger.info("channel joind to conference and it is the only one channel in it");
+				new Play(cc, "conf-onlyperson").run();
+			} else {
+				logger.info("channel joind to conference");
+				// play to all channels in the bridge- new PlayBridge class and in run method:
+				// getAri().bridges().play(getBridgeId(), fullPath,
+				// callStasisStart.getChannel().getLanguage(), 0, 0,playbackId, new
+				// AriCallback<Playback>() {
+			}
+			if (count == 2)
+				currState = ConferenceState.Ready;
 		}
-		channelsInConf.add(channel);
-		count++;
-		if(count == 1) {
-			logger.info("channel joind to conference and it is the only one channel in it");
-			new Play(cc, "conf-onlyperson").run();
-		}
-		else {
-			logger.info("channel joind to conference");
-			//play to all channels in the bridge- new PlayBridge class and in run method: 
-			//getAri().bridges().play(getBridgeId(), fullPath, callStasisStart.getChannel().getLanguage(), 0, 0,playbackId, new AriCallback<Playback>() {
-		}
-		if(count == 2)
-			currState = ConferenceState.Ready;
+		else
+			logger.info("unable to add channel to conference. conference state is: " + currState);
 	}
-	
+
 	/**
 	 * remove channel that left the conference
-	 * @param channel channel to be removed
+	 * 
+	 * @param channel
+	 *            channel to be removed
 	 */
 	public void removeChannelFromConf(Channel channel) {
 		count--;
@@ -152,6 +179,5 @@ public class Conference extends Operation {
 			compFuture.complete(this);
 		}
 	}
-
 
 }
