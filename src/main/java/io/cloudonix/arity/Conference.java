e2c59afb0bb5e2300cca9c5ffabf2aeffa6320f1
Naama Gertel <naama,gertel@cloudonix.io>
2018-04-11 19:18:50 +0300

    start working on run method of COnference. not done

--- src/main/java/io/cloudonix/arity/Conference.java
+++ src/main/java/io/cloudonix/arity/Conference.java
@@ -9,166 +9,157 @@ import java.util.logging.Logger;
 import ch.loway.oss.ari4java.ARI;
 import ch.loway.oss.ari4java.generated.Bridge;
 import ch.loway.oss.ari4java.generated.Channel;
+import ch.loway.oss.ari4java.generated.ari_2_0_0.models.ChannelLeftBridge_impl_ari_2_0_0;
 import ch.loway.oss.ari4java.tools.RestException;
 import io.cloudonix.arity.errors.ErrorStream;
 
 public class Conference extends Operation {
-
-	public enum ConferenceState {
-		Destroyed, Destroying, Creating, Ready, ReadyWaiting, Muted, AdminMuted
-	}
-
+	
+	public enum ConferenceState
+    {
+        Destroyed, Destroying ,Creating, Ready, ReadyWaiting, Muted, AdminMuted 
+    }
+	private ARIty arity;
 	private CompletableFuture<Conference> compFuture;
 	private ConferenceState currState;
 	private Bridge confBridge;
 	private String confName;
 	private int count = 0;
 	private List<Channel> channelsInConf;
-	private String confId;
 	private final static Logger logger = Logger.getLogger(Conference.class.getName());
 
-	public Conference(String confId, ARIty arity, ARI ari) {
-		super(confId, arity, ari);
-		this.confId = confId;
+	
+	public Conference(String confId, ARIty a, ARI ari) {
+		super(confId, a, ari);
+		arity = a;
 		channelsInConf = new ArrayList<>();
 		compFuture = new CompletableFuture<>();
 	}
 
 	@Override
 	public CompletableFuture<? extends Operation> run() {
+		/*if(Objects.equals(currState, ConferenceState.Ready)) {
+			return this.<Void>toFuture(addChannel-> addChannelToConf(channel , getArity().getCallSupplier().get()))
+					.thenAccept(v->{
+						for(int i = 0; i <channelsInConf.size(); i++) {
+							getArity().addFutureEvent(ChannelLeftBridge_impl_ari_2_0_0.class, (chanLeftBridge)-> {
+								removeChannelFromConf(channelsInConf.get(i));
+								if(channelsInConf.size() < 2) {
+									logger.info("conference must contain at least 2 participantes. closing the conference");
+									compFuture.complete(this);
+								}
+							});
+						}
+					}).thenCompose(v->compFuture);
+					
+		}*/
 		return null;
 	}
-
+	
 	/**
 	 * get conference currenr state
-	 * 
 	 * @return
 	 */
 	public ConferenceState getCurrState() {
 		return currState;
 	}
-
+	
 	/**
 	 * set conference state
-	 * 
-	 * @param currState
-	 *            update sate of conference
-	 */
+	 * @param currState  update sate of conference
+	 * 	 */
 	public void setCurrState(ConferenceState currState) {
 		this.currState = currState;
 	}
-
 	/**
 	 * get conference bridge
-	 * 
 	 * @return
 	 */
 	public Bridge getConfBridge() {
 		return confBridge;
 	}
-
 	/**
 	 * set conference bridge
-	 * 
 	 * @return
 	 */
 	public void setConfBridge(Bridge confBridge) {
 		this.confBridge = confBridge;
 	}
-
 	/**
 	 * get conference name
-	 * 
 	 * @return
 	 */
 	public String getConfName() {
 		return confName;
 	}
-
 	/**
 	 * set conference name
-	 * 
 	 * @return
 	 */
 	public void setConfName(String confName) {
 		this.confName = confName;
 	}
-
+	
 	/**
 	 * get number of channels in conference
-	 * 
 	 * @return
 	 */
 	public int getCount() {
 		return count;
 	}
-
 	/**
 	 * set number of channels in conference
-	 * 
 	 * @return
 	 */
 	public void setCount(int count) {
 		this.count = count;
 	}
-
+	
 	/**
 	 * get list of channels connected to the conference bridge
-	 * 
 	 * @return
 	 */
 	public List<Channel> getChannelsInConf() {
 		return channelsInConf;
 	}
-
 	/**
 	 * set list of channels connected to the conference bridge
-	 * 
 	 * @return
 	 */
 	public void setChannelsInConf(List<Channel> channelsInConf) {
 		this.channelsInConf = channelsInConf;
 	}
-
 	/**
 	 * add channel to the conference
-	 * 
 	 * @param channel
 	 * @param cc
 	 */
-	public void addChannelToConf(Channel channel, CallController cc) {
-		
-		if (Objects.equals(currState, ConferenceState.Ready)) {
-			try {
-				cc.getAri().bridges().addChannel(confBridge.getId(), channel.getId(), "joining conference");
-			} catch (RestException e) {
-				logger.severe("unable to add channel to conference " + ErrorStream.fromThrowable(e));
-				return;
-			}
-			channelsInConf.add(channel);
-			count++;
-			if (count == 1) {
-				logger.info("channel joind to conference and it is the only one channel in it");
-				new Play(cc, "conf-onlyperson").run();
-			} else {
-				logger.info("channel joind to conference");
-				// play to all channels in the bridge- new PlayBridge class and in run method:
-				// getAri().bridges().play(getBridgeId(), fullPath,
-				// callStasisStart.getChannel().getLanguage(), 0, 0,playbackId, new
-				// AriCallback<Playback>() {
-			}
-			if (count == 2)
-				currState = ConferenceState.Ready;
+	public void addChannelToConf (Channel channel, CallController cc) {
+		try {
+			cc.getAri().bridges().addChannel(confBridge.getId(), channel.getId(), "joining conference");
+		} catch (RestException e) {
+			logger.severe("unable to add channel to conference " +ErrorStream.fromThrowable(e));
+			return;
 		}
-		else
-			logger.info("unable to add channel to conference. conference state is: " + currState);
+		channelsInConf.add(channel);
+		count++;
+		
+		if(count == 1) {
+			logger.info("channel joind to conference and it is the only one channel in it");
+			new Play(cc, "conf-onlyperson").run();
+		}
+		else {
+			logger.info("channel joind to conference");
+			//play to all channels in the bridge- new PlayBridge class and in run method: 
+			//getAri().bridges().play(getBridgeId(), fullPath, callStasisStart.getChannel().getLanguage(), 0, 0,playbackId, new AriCallback<Playback>() {
+		}
+		if(count == 2)
+			currState = ConferenceState.Ready;
 	}
-
+	
 	/**
 	 * remove channel that left the conference
-	 * 
 	 * @param channel
-	 *            channel to be removed
 	 */
 	public void removeChannelFromConf(Channel channel) {
 		count--;
@@ -179,5 +170,6 @@ public class Conference extends Operation {
 			compFuture.complete(this);
 		}
 	}
+
 
 }
