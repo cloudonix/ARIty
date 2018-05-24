93f03535bca74637a57637e7a1e9850788b65c9c
Naama Gertel <naama,gertel@cloudonix.io>
2018-05-24 10:56:30 +0300

    removed all code related to a bridge and changed dial to originate instead (checked all 3 possible cases of hang up)

--- src/main/java/io/cloudonix/arity/Dial.java
+++ src/main/java/io/cloudonix/arity/Dial.java
@@ -9,18 +9,10 @@ import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
-
-import ch.loway.oss.ari4java.generated.Bridge;
-import ch.loway.oss.ari4java.generated.BridgeCreated;
 import ch.loway.oss.ari4java.generated.Channel;
 import ch.loway.oss.ari4java.generated.ChannelHangupRequest;
-import ch.loway.oss.ari4java.generated.ChannelLeftBridge;
-import ch.loway.oss.ari4java.generated.ari_2_0_0.models.ChannelEnteredBridge_impl_ari_2_0_0;
 import ch.loway.oss.ari4java.generated.ari_2_0_0.models.Dial_impl_ari_2_0_0;
-import ch.loway.oss.ari4java.tools.AriCallback;
 import ch.loway.oss.ari4java.tools.RestException;
-import io.cloudonix.arity.Conference.ConferenceState;
-import io.cloudonix.arity.errors.ErrorStream;
 import io.cloudonix.arity.errors.HangUpException;
 
 /**
@@ -103,26 +95,11 @@ public class Dial extends CancelableOperations {
 	 */
 	public CompletableFuture<Dial> run() {
 		endPointChannelId = UUID.randomUUID().toString();
-		String bridgeID = UUID.randomUUID().toString();
 
 		// add the new channel channel id to the set of ignored Channels
 		getArity().ignoreChannel(endPointChannelId);
 		getArity().addFutureEvent(ChannelHangupRequest.class, this::handleHangup);
 		logger.info("future event of ChannelHangupRequest was added");
-
-		getArity().addFutureEvent(BridgeCreated.class, this::bridgeCreated);
-		logger.info("future event of BridgeCreated was added");
-
-		getArity().addFutureEvent(ChannelEnteredBridge_impl_ari_2_0_0.class, (chanInBridge) -> {
-			if (Objects.equals(chanInBridge.getChannel().getId(), getChannelId())) {
-				logger.info("try adding caller to conference");
-				return channelEnteredBridge(chanInBridge);
-			}
-			if (Objects.equals(chanInBridge.getChannel().getId(), endPointChannelId))
-				return channelEnteredBridge(chanInBridge);
-			return false;
-		});
-		logger.info("future event of ChannelEnteredBridge was added");
 
 		getArity().addFutureEvent(Dial_impl_ari_2_0_0.class, (dial) -> {
 			dialStatus = dial.getDialstatus();
@@ -130,62 +107,18 @@ public class Dial extends CancelableOperations {
 			if (!dialStatus.equals("ANSWER"))
 				return false;
 			mediaLenStart = Instant.now();
-			compFuture.complete(this);
 			return true;
 		});
 		logger.info("future event of Dial was added");
-		
-		return this.<Bridge>toFuture(cf -> getAri().bridges().create("", bridgeID, bridgeName, cf))
-				.thenAccept(bridge -> {
-					try {
-						getAri().bridges().addChannel(bridge.getId(), getChannelId(), "caller");
-						logger.info(" Caller's channel was added to the bridge. Channel id of the caller:" + getChannelId());
 
-						//getAri().channels().create(endPoint, getArity().getAppName(), null, endPointChannelId, null,getChannelId(), null);
-
-						getAri().channels().originate(endPoint, null,null, 1, null, getArity().getAppName(), null, callerId, -1, headers, endPointChannelId, null, getChannelId(), "", new AriCallback<Channel>() {
-							
-							@Override
-							public void onSuccess(Channel result) {
-								logger.info("endpoint channel was created");
-							}
-							
-							@Override
-							public void onFailure(RestException e) {
-								logger.info("unable to originate endpoint channel "+ErrorStream.fromThrowable(e));
-								compFuture.completeExceptionally(e);								
-							}
-						});
-
-
-					} catch (RestException e2) {
-						logger.info("failed dailing " + e2);
-						compFuture.completeExceptionally(e2);
-					}
-				}).thenAccept(v -> {
+		return this
+				.<Channel>toFuture(
+						cf -> getAri().channels().originate(endPoint, null, null, 1, null, getArity().getAppName(),
+								null, callerId, -1, headers, endPointChannelId, null, getChannelId(), "", cf))
+				.thenAccept(v -> {
 					logger.info("dial succeded!");
 					dialStart = Instant.now().toEpochMilli();
 				}).thenCompose(v -> compFuture);
-	}
-
-	/**
-	 * handle bridge created event
-	 * 
-	 * @param bridge
-	 *            BridgeCreated event
-	 * @return
-	 */
-	private boolean bridgeCreated(BridgeCreated bridge) {
-		if (Objects.nonNull(bridge.getBridge())) {
-			Conference conference = new Conference(UUID.randomUUID().toString(), getArity(), getAri());
-			conference.setConfBridge(bridge.getBridge());
-			logger.info("brige id: " + bridge.getBridge().getId());
-			conferences.add(conference);
-			logger.info("confrence bridge was saved");
-			return true;
-		}
-		logger.severe("conference bridge was not created");
-		return false;
 	}
 
 	/**
@@ -197,32 +130,6 @@ public class Dial extends CancelableOperations {
 	 */
 	private Boolean handleHangup(ChannelHangupRequest hangup) {
 		// notice when a channel leaves a bridge after a hang up occurred
-		getArity().addFutureEvent(ChannelLeftBridge.class, (channelLeft) -> {
-			if (conferences.size() == 1 && conferences.get(0).getChannelsInConf().size() == 0) {
-				logger.info("1 conference with no participents. remove it");
-				conferences.remove(0);
-				getArity().getCallSupplier().get().setConferences(conferences);
-				return true;
-			}
-			for (int i = 0; i < conferences.size(); i++) {
-				for (int j = 0; j < conferences.get(i).getChannelsInConf().size(); j++) {
-					if (Objects.equals(conferences.get(i).getChannelsInConf().get(j).getId(),
-							channelLeft.getChannel().getId())) {
-						logger.info("removing channel: " + channelLeft.getChannel().getId() + " from conference");
-						conferences.get(i).removeChannelFromConf(channelLeft.getChannel());
-						if (conferences.get(i).getCount() < 2) {
-							logger.info("conference call was completed, remove it from the list");
-							conferences.remove(i);
-							getArity().getCallSupplier().get().setConferences(conferences);
-							return true;
-						}
-					}
-				}
-			}
-			logger.info("no conference contains the channel: " + channelLeft.getChannel().getId());
-			return false;
-		});
-		logger.info("future event of ChannelLeftBridge_impl_ari_2_0_0.class was added");
 
 		if (hangup.getChannel().getId().equals(getChannelId()) && !isCanceled) {
 			if (Objects.equals(dialStatus, "ANSWER")) {
@@ -235,7 +142,7 @@ public class Dial extends CancelableOperations {
 			return false;
 		}
 
-		if (!(hangup.getChannel().getId().equals(endPointChannelId)))
+		if (!(hangup.getChannel().getId().equals(endPointChannelId))) 
 			return false;
 
 		if (!isCanceled || Objects.equals(dialStatus, "ANSWER")) {
@@ -245,40 +152,9 @@ public class Dial extends CancelableOperations {
 			logger.info("duration of the call: " + callDuration + " ms");
 			mediaLength = Math.abs(end.toEpochMilli() - mediaLenStart.toEpochMilli());
 			logger.info("media lenght of the call: " + mediaLength + " ms");
-			logger.info("end point channel hanged up");
 		}
-
 		compFuture.complete(this);
 		return true;
-	}
-
-	/**
-	 * handle a channel entered bridge event
-	 * 
-	 * @param chanInBridge
-	 *            instance of channelEnteredBridge event
-	 * @param channelId
-	 * @return
-	 */
-	private boolean channelEnteredBridge(ChannelEnteredBridge_impl_ari_2_0_0 chanInBridge) {
-		for (int i = 0; i < conferences.size(); i++) {
-			if (Objects.equals(conferences.get(i).getConfBridge().getId(), chanInBridge.getBridge().getId())) {
-				conferences.get(i).setNewChannel(chanInBridge.getChannel());
-				conferences.get(i).setConfName(bridgeName);
-				conferences.get(i).addChannelToConf(chanInBridge.getChannel());
-				logger.info("channel: " + chanInBridge.getChannel().getId() + " was added to confrence: "
-						+ conferences.get(i).getConfName());
-				if (conferences.get(i).getCount() == 2) {
-					conferences.get(i).setCurrState(ConferenceState.Ready);
-					// update call controller conferences list as well
-					getArity().getCallSupplier().get().setConferences(conferences);
-					logger.info("conference is ready");
-					return true;
-				}
-				return true;
-			}
-		}
-		return false;
 	}
 
 	/**
