2c7a7b4404b10dff40707404d9a6c91dc90154bb
Naama Gertel <naama,gertel@cloudonix.io>
2018-06-03 11:43:31 +0300

    removed unused imports from Dial

--- src/main/java/io/cloudonix/arity/Dial.java
+++ src/main/java/io/cloudonix/arity/Dial.java
@@ -1,8 +1,6 @@
 package io.cloudonix.arity;
 
 import java.time.Instant;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Objects;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
