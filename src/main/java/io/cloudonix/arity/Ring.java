3031829053831b3b2e537cbc683c63402bea2d45
Naama Gertel <naama,gertel@cloudonix.io>
2018-07-24 10:25:04 +0300

    ringing is a cancelableOperation

--- src/main/java/io/cloudonix/arity/Ring.java
+++ src/main/java/io/cloudonix/arity/Ring.java
@@ -8,7 +8,7 @@ import io.cloudonix.arity.errors.RingException;
 import io.cloudonix.future.helper.FutureHelper;
 
 
-public class Ring  extends Operation{
+public class Ring  extends CancelableOperations{
 	
 	private final static Logger logger = Logger.getLogger(Answer.class.getName());
 	private String channelId;
@@ -29,6 +29,17 @@ public class Ring  extends Operation{
 			logger.severe("Unable to ring to channel with id: "+ channelId+" :"+e);
 			return FutureHelper.completedExceptionally(new RingException(e));
 		}
+		logger.info("Ringing to channel with id: "+channelId);
 		return CompletableFuture.completedFuture(this);
+	}
+
+	@Override
+	void cancel() {
+		try {
+			getAri().channels().ringStop(channelId);
+		} catch (RestException e) {
+			logger.warning("Unable to stop ringing to channel with id "+ channelId+" : "+e);
+		}
+		logger.info("Stoped ringing to channel with id: "+channelId);
 	}
 }
