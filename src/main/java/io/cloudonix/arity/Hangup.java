e4b034e6ccdb88aa0ed8f9a8128dc2ed3852d17e
Naama Gertel <naama,gertel@cloudonix.io>
2018-08-20 12:00:01 +0300

    support hang up of another channel

--- src/main/java/io/cloudonix/arity/Hangup.java
+++ src/main/java/io/cloudonix/arity/Hangup.java
@@ -1,50 +1,66 @@
 package io.cloudonix.arity;
 
+import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.logging.Logger;
 
 import ch.loway.oss.ari4java.tools.AriCallback;
 import ch.loway.oss.ari4java.tools.RestException;
 
-
 /**
  * The class represents the Hang up operation (hangs up a call)
+ * 
  * @author naamag
  *
  */
-public class Hangup extends Operation{
-	
+public class Hangup extends Operation {
+
 	private final static Logger logger = Logger.getLogger(Hangup.class.getName());
-	
+	private String channelId = "";
+
 	/**
 	 * Constructor
-	 * @param callController instance that represents a call
+	 * 
+	 * @param callController
+	 *            instance that represents a call
 	 */
 	public Hangup(CallController callController) {
 		super(callController.getChannelID(), callController.getARItyService(), callController.getAri());
 	}
-	
+
 	/**
-	 * The method hangs up a call
+	 * The method hangs up a channel
 	 * 
 	 * @return
 	 */
-	public CompletableFuture<Hangup> run (){
+	public CompletableFuture<Hangup> run() {
 		CompletableFuture<Hangup> future = new CompletableFuture<Hangup>();
-		getAri().channels().hangup(getChannelId(),"normal", new AriCallback<Void>() {
-			
+		String id = (Objects.equals(channelId, "")) ? getChannelId() : channelId;
+		getAri().channels().hangup(id, "normal", new AriCallback<Void>() {
+
 			@Override
 			public void onSuccess(Void result) {
-				logger.info("Hanged up channel with id: "+ getChannelId());
+				logger.info("Hanged up channel with id: " + getChannelId());
 				future.complete(null);
 			}
-			
+
 			@Override
 			public void onFailure(RestException e) {
-				logger.warning("Failed hang up channel with id: "+ getChannelId());
+				logger.warning("Failed hang up channel with id: " + getChannelId());
 				future.complete(null);
 			}
 		});
 		return future;
+	}
+
+	/**
+	 * set the channel id when need to hang up another channel
+	 * 
+	 * @param channelId
+	 *            the id of the channel to be hanged up
+	 * 
+	 */
+	public void setChannelId(String channelId) {
+		this.channelId = channelId;
 	}
 }
