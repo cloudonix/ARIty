f3aba8ed7fea9bc7c541e74798588d44f8e5e70c
Naama Gertel <naama,gertel@cloudonix.io>
2018-12-05 19:34:41 +0200

    no need to save the channelVarset because it does not give any useful information

--- src/main/java/io/cloudonix/arity/CallState.java
+++ src/main/java/io/cloudonix/arity/CallState.java
@@ -1,15 +1,10 @@
 package io.cloudonix.arity;
 
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Objects;
-import java.util.logging.Logger;
 
 import ch.loway.oss.ari4java.ARI;
 import ch.loway.oss.ari4java.generated.Channel;
-import ch.loway.oss.ari4java.generated.ChannelVarset;
 import ch.loway.oss.ari4java.generated.StasisStart;
 
 /**
@@ -27,8 +22,6 @@ public class CallState {
 	private Channel channel;
 	private String channelTechnology;
 	private Map<String, Object> metaData = new HashMap<>();
-	private Logger logger = Logger.getLogger(getClass().getName());
-	private List<ChannelVarset> allChannelVarSet = Collections.emptyList();
 
 	public CallState(StasisStart callStasisStart, ARI ari, ARIty arity, String channelID, Channel channel,
 			String channelTechnology) {
@@ -38,21 +31,6 @@ public class CallState {
 		this.channel = channel;
 		this.channelTechnology = channelTechnology;
 		this.callStasisStart = callStasisStart;
-		arity.addFutureEvent(ChannelVarset.class, channelID, this::saveChannelVarset, false);
-		logger = Logger.getLogger(getClass().getName() + ":" + channelID);
-	}
-
-	/**
-	 * save ChannelVarset events
-	 * 
-	 * @param cvs ChannelVarset event
-	 * @return
-	 */
-	public Boolean saveChannelVarset(ChannelVarset cvs) {
-		logger.info("Got ChannelVarset. Variable " + cvs.getVariable() + " has changed and new value is: "
-				+ cvs.getValue());
-		allChannelVarSet.add(cvs);
-		return false;
 	}
 
 	public StasisStart getCallStasisStart() {
@@ -108,23 +86,5 @@ public class CallState {
 	 */
 	public String getChannelTechnology() {
 		return channelTechnology;
-	}
-
-	public List<ChannelVarset> getLatestChannelVarSet() {
-		return allChannelVarSet;
-	}
-
-	/**
-	 * get the variable's value of the channel
-	 * 
-	 * @param variable name of the variable
-	 * @return the value of the variable if exists, or "Not found" otherwise
-	 */
-	public String getValueOfVariable(String variable) {
-		for (ChannelVarset cvs : allChannelVarSet) {
-			if (Objects.equals(variable, cvs.getVariable()))
-				return cvs.getValue();
-		}
-		return "Not found";
 	}
 }
