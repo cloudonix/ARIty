48346c0e0b3f6c967acab720a81a38d45eba4cd9
Naama Gertel <naama,gertel@cloudonix.io>
2019-01-03 11:40:01 +0200

    ReceivedDTMF is not an operation! and no need time limit

--- src/main/java/io/cloudonix/arity/ReceivedDTMF.java
+++ src/main/java/io/cloudonix/arity/ReceivedDTMF.java
@@ -10,12 +10,12 @@ import ch.loway.oss.ari4java.generated.ChannelDtmfReceived;
 import io.cloudonix.future.helper.FutureHelper;
 
 /**
- * The class represents the Received DTMF operation
+ * The class represents the Received DTMF events
  * 
  * @author naamag
  *
  */
-public class ReceivedDTMF extends CancelableOperations {
+public class ReceivedDTMF {
 	private String userInput = "";
 	private final static Logger logger = Logger.getLogger(ReceivedDTMF.class.getName());
 	private List<CancelableOperations> nestedOperations = new ArrayList<>();;
@@ -27,7 +27,8 @@ public class ReceivedDTMF extends CancelableOperations {
 	private boolean termKeyWasPressed = false;
 	private boolean doneAllOps = false;
 	private CompletableFuture<ReceivedDTMF> compFuture = new CompletableFuture<>();
-	private int timeLimit = Integer.MAX_VALUE; // in ms
+	private ARIty arity;
+	private String channelId;
 
 	/**
 	 * Constructor
@@ -36,14 +37,13 @@ public class ReceivedDTMF extends CancelableOperations {
 	 * @param termKey        define terminating key (otherwise '#' is the default)
 	 * @param length         length of the input we are expecting to get from the
 	 *                       caller. for no limitation -1
-	 * @param timeLimit limit the time we allowed the DTMF to receive. For no limit, -1
 	 */
-	public ReceivedDTMF(CallController callController, String termKey, int length, int timeLimit) {
-		super(callController.getChannelID(), callController.getARItyService(), callController.getAri());
+	public ReceivedDTMF(CallController callController, String termKey, int length) {
 		this.terminatingKey = termKey;
 		this.inputLength = length;
-		if(timeLimit != -1)
-			this.timeLimit = timeLimit;
+		this.arity = callController.getARItyService();
+		this.channelId = callController.getChannelID();
+		
 	}
 
 	/**
@@ -52,7 +52,7 @@ public class ReceivedDTMF extends CancelableOperations {
 	 * @param callController
 	 */
 	public ReceivedDTMF(CallController callController) {
-		this(callController, "#", -1,-1);
+		this(callController, "#", -1);
 	}
 
 	/**
@@ -61,7 +61,7 @@ public class ReceivedDTMF extends CancelableOperations {
 	 * @return
 	 */
 	public CompletableFuture<ReceivedDTMF> run() {
-		getArity().addFutureEvent(ChannelDtmfReceived.class, getChannelId(), this::handleDTMF);
+		arity.addFutureEvent(ChannelDtmfReceived.class, channelId, this::handleDTMF);
 
 		if (!nestedOperations.isEmpty()) {
 			logger.fine("there are verbs in the nested operation list");
@@ -91,12 +91,6 @@ public class ReceivedDTMF extends CancelableOperations {
 	 * @param se the saved event handler for dtmf
 	 */
 	public void handleDTMF(ChannelDtmfReceived dtmf, SavedEvent<ChannelDtmfReceived>se) {
-		if(dtmf.getDuration_ms() > timeLimit) {
-			logger.fine("Time limit to receive DTMF reached, done receiving DTMF");
-			compFuture.complete(this);
-			se.unregister();
-			return;
-		}
 		if (dtmf.getDigit().equals(terminatingKey)) {
 			logger.info("Done receiving DTMF. all input: " + userInput);
 			termKeyWasPressed = true;
@@ -172,15 +166,6 @@ public class ReceivedDTMF extends CancelableOperations {
 
 	public void setDoneAllOps(boolean doneAllOps) {
 		this.doneAllOps = doneAllOps;
-	}
-
-	@Override
-	public CompletableFuture<Void> cancel() {
-		logger.info("Cancelling receiving DTMF");
-		isCanceled = true;
-		return cancelAll().thenAccept(v -> {
-			compFuture.complete(this);
-		});
 	}
 
 	public boolean isCanceled() {
