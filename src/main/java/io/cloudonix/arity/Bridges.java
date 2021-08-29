767c33545cbe6f500edbe2aad0ec04a2635ab810
Oded Arbel <oded@geek.co.il>
2021-08-29 20:35:40 +0300

    allow bridge types to be specified, but ensure at least "mixing" is still provided

--- src/main/java/io/cloudonix/arity/Bridges.java
+++ src/main/java/io/cloudonix/arity/Bridges.java
@@ -1,12 +1,35 @@
 package io.cloudonix.arity;
 
+import java.util.HashSet;
+import java.util.Set;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import ch.loway.oss.ari4java.generated.actions.ActionBridges;
 import io.cloudonix.arity.models.AsteriskBridge;
 
 public class Bridges {
+	
+	public enum BridgeType {
+		mixing, holding, dtmf_events, proxy_media, video_sfu, video_single;
+
+		public static BridgeType[] merge(BridgeType[] types, BridgeType ...othertypes) {
+			var stypes = new HashSet<>(Set.of(types));
+			for (var o : othertypes) {
+				switch (o) {
+				case mixing:
+				case holding:
+					if (stypes.contains(BridgeType.mixing) || stypes.contains(BridgeType.holding))
+						continue;
+				default:
+					stypes.add(o);
+				}
+			}
+			return stypes.toArray(new BridgeType[] {});
+		}
+	}
 
 	private ARIty arity;
 	private ActionBridges api;
@@ -16,20 +39,18 @@ public class Bridges {
 		this.api = arity.getAri().bridges();
 	}
 
-	public CompletableFuture<AsteriskBridge> create() {
+	public CompletableFuture<AsteriskBridge> create(BridgeType... types) {
 		String id = UUID.randomUUID().toString();
-		return create(id, id);
+		return create(id, id, types);
 	}
 	
-	public CompletableFuture<AsteriskBridge> create(String bridgeName) {
-		return create(UUID.randomUUID().toString(), bridgeName);
+	public CompletableFuture<AsteriskBridge> create(String bridgeName, BridgeType... types) {
+		return create(UUID.randomUUID().toString(), bridgeName, types);
 	}
 
-	public CompletableFuture<AsteriskBridge> create(String bridgeId, String bridgeName) {
-		return create(bridgeId, bridgeName, "mixing");
-	}
-	
-	public CompletableFuture<AsteriskBridge> create(String bridgeId, String bridgeName, String bridgeType) {
+	public CompletableFuture<AsteriskBridge> create(String bridgeId, String bridgeName, BridgeType... types) {
+		var bridgeTypes = BridgeType.merge(types, BridgeType.mixing); // make sure that types includes either "mixing" or "holding"
+		var bridgeType = Stream.of(bridgeTypes).map(Object::toString).collect(Collectors.joining(","));
 		return Operation.<ch.loway.oss.ari4java.generated.models.Bridge>retry(cb -> api.create()
 				.setBridgeId(bridgeId).setName(bridgeName).setType(bridgeType).execute(cb))
 				.thenApply(b -> new AsteriskBridge(arity, b));
